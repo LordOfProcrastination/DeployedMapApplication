01344e7d1841d80019be16b0eecb3b2192035010
LordOfProcrastination <henrik_aulie@hotmail.no>
2024-02-25 19:38:45 +0100

    Fixed capitalisation

--- src/modules/application/application.tsx
+++ src/modules/application/application.tsx
@@ -7,7 +7,7 @@ import "ol/ol.css";
 import { map, MapContext } from "../map/mapContext";
 import { Layer } from "ol/layer";
 import { CivilDefenceCheckbox } from "../civildefence/civilDefenceLayer";
-import { EmergencyLayerCheckbox } from "../emergencyshelters/emergencyshelterCheckbox";
+import { EmergencyLayerCheckbox } from "../emergencyShelters/emergencyShelterCheckbox";
 
 export function Application() {
   function handleFocusUser(e: React.MouseEvent) {
